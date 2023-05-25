198f1185fbfa11793619f5af62c71e7a762661f0
Leiner Johan Mosquera Aranda <leiner.mosquera@globant.com>
2023-05-25 17:52:22 -0500

    Feat: se agrega componente search usando material 3

--- app/src/main/java/com/example/musicchallenge/presentation/SharedViewModel.kt
+++ app/src/main/java/com/example/musicchallenge/presentation/SharedViewModel.kt
@@ -7,29 +7,86 @@ import androidx.lifecycle.viewModelScope
 import com.example.musicchallenge.domain.models.Song
 import com.example.musicchallenge.domain.usesCases.SongUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
+@OptIn(FlowPreview::class)
 class SharedViewModel @Inject constructor(
     private var songUseCase: SongUseCase
 ) : ViewModel() {
 
 
-    //Definition of Livedata for movies by query.
-    private val _songs = MutableLiveData<List<Song>>()
-    val songs: LiveData<List<Song>> = _songs
+    private val _searchText = MutableStateFlow("")
+    var searchText = _searchText.asStateFlow()
 
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+    private val _songs = MutableStateFlow(allSongs)//listOf<Song>())
+    val songs = searchText.combine(_songs) { text, songs ->
+        if (text.isBlank()) {
+            songs
+        } else {
+            delay(2000L)
+            songs.filter{
+                it.doesMatchSearchQuery(text)
+            }
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        _songs.value
+    )
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
+/*
     fun getSongsBySearch(query: String) {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 if (!query.isNullOrEmpty()) {
-                    _songs.postValue(songUseCase.getSongsBySearch(query))
+                    //_songs.postValue(songUseCase.getSongsBySearch(query))
                 }
             }
         }
-    }
+    }*/
 
 }
+data class Song(
+    val firstName: String,
+    val lastName: String
+) {
+    fun doesMatchSearchQuery(query: String): Boolean {
+        val matchingCombinations = listOf(
+            "$firstName$lastName",
+            "$firstName $lastName",
+            "${firstName.first()} ${lastName.first()}",
+        )
+
+        return matchingCombinations.any {
+            it.contains(query, ignoreCase = true)
+        }
+    }
+}
+
+private val allSongs = listOf(
+    Song(
+        firstName = "Philipp",
+        lastName = "Lackner"
+    ),
+    Song(
+        firstName = "Beff",
+        lastName = "Jezos"
+    ),
+    Song(
+        firstName = "Chris P.",
+        lastName = "Bacon"
+    ),
+    Song(
+        firstName = "Jeve",
+        lastName = "Stops"
+    ),
+)
--- app/src/main/java/com/example/musicchallenge/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicchallenge/presentation/ui/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.musicchallenge.presentation.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicchallenge.presentation.SharedViewModel
 import com.example.musicchallenge.presentation.ui.theme.MusicChallengeTheme
-import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -25,7 +26,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("MainActivity", "main activity launched")
+
+        /*Log.d("MainActivity", "main activity launched")
         sharedViewModel.songs.observe(this, Observer { songs ->
             // Process the observed songs list
             // Print the list to the console
@@ -33,30 +35,66 @@ class MainActivity : ComponentActivity() {
                 println(song.toString())
             }
         })
-        sharedViewModel.getSongsBySearch("shakira")
+        sharedViewModel.getSongsBySearch("shakira")*/
         setContent {
             MusicChallengeTheme {
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(sharedViewModel = sharedViewModel)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainScreen(sharedViewModel: SharedViewModel) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MusicChallengeTheme {
-        Greeting("Android")
+    val searchText by sharedViewModel.searchText.collectAsState()
+    val songs by sharedViewModel.songs.collectAsState()
+    val isSearching by sharedViewModel.isSearching.collectAsState()
+    var active by remember { mutableStateOf(false) }
+
+    Scaffold {
+        SearchBar(
+            modifier = Modifier.fillMaxSize(),
+            query = searchText,
+            onQueryChange = sharedViewModel::onSearchTextChange,
+            onSearch = {
+                active = false
+            },
+            active = active,
+            onActiveChange = {
+                active = it
+            },
+            placeholder = { Text(text = "Search") },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+            },
+            trailingIcon = {
+                if (active) {
+                    Icon(
+                        modifier = Modifier.clickable {
+                            if (searchText.isNotEmpty()) {
+                                
+                            }
+                        },
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close"
+                    )
+                }
+
+            }
+        ) {
+
+        }
+
     }
 }
